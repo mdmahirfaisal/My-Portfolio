cfdac10bd707511405e0402b19cbae644641e914
unknown <rjmahir.faisal@gmail.com>
2022-02-03 21:03:22 +0600

    about page resume added

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,6 +3,7 @@ import './About.css';
 import myImg from './img/faisal.png';
 import Fade from 'react-reveal/Fade';
 import Flip from 'react-reveal/Flip';
+import { Nav } from 'react-bootstrap';
 
 
 
@@ -28,36 +29,13 @@ const About = () => {
                                     <h4 className="about-text text-start"><Flip cascade duration={2000}> Hi there, I' am</Flip></h4>
                                     <h1 className="about-text text-start  ms-0"><Flip cascade duration={2000}>Mahir Faisal</Flip></h1>
                                     <p className="about-text text-start"><i><Flip cascade duration={2000}>I am a junior front-end web developer I am working with react.js. I have a good grasp of HTML5 CSS3 JAVASCRIPT. And I also did some projects with Node.js Express.js MongoDB in the backend I have 5 Plus project experience. Thank you.</Flip></i></p>
-                                    <button className='btn btn-success fs-4'>Resume</button>
+                                    <Nav.Link className="fs-4" as="a" href={`https://drive.google.com/file/d/1IWDXgsTltXLd81c202tLphDiYI-JgIIW/view?usp=sharing`} target="_blank" rel="noopener noreferrer"><button className="btn btn-success py-1 ">Resume</button></Nav.Link>
                                 </div>
                             </div>
                         </Fade>
                     </div>
                 </div>
             </section>
-
-
-
-
-            {/* <section class="about about-section" id="about">
-                <div class="content">
-                    <div class="title"><span>About Me</span></div>
-                    <div class="about-details">
-                        <div class="left">
-                            <img src={myImg} alt="Faisal" />
-                        </div>
-                        <div class="right">
-                            <div class="topic">Designing Is My Passion</div>
-                            <p>Welcome to my mini portfolio website! My name is Ananthu A Nair. I am a frontend developer and currently staying in India. I am a person, who has high passion in web development, game development. One of my dreams is to master all the  technologies and become one of the TOP developer in the world, and now, I am working toward it!</p>
-                            <div class="button">
-                                <button >Download CV</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section> */}
-
-
         </>
     );
 };
